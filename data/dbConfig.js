d9c89deef93274dd69fd1c0be0a84da32ffa68ae
RichVI <richardwang2050@gmail.com>
2020-05-26 12:53:11 -0700

    server v.1.1

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,6 @@
 const knex = require("knex");
+
 const env = process.env.NODE_ENV || "development";
 const knexfile = require("../knexfile.js");
 
-module.exports = knex(knexfile[env]); //sets configuration based on env variable
+module.exports = require("knex")(config); //sets configuration based on env variable
